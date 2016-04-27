f3214fa35896f1f58db4efbbc41b7a7a09ff6082
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-04-27 13:45:12 +0200

    Conversion md -> pdf

--- md2pandoc.sh
+++ md2pandoc.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-pandoc -i "$@" -f markdown -t latex --latex-engine=xelatex  -s -o "$@".pdf
+pandoc "$@" -f markdown -t latex --latex-engine=xelatex  -s -o "$@".pdf
 
 #Script pour transformer les fichiers Markdown en pdf via LaTeX. Ça devrait marcher, ou bien c'est quelque chose de pas trop différent
